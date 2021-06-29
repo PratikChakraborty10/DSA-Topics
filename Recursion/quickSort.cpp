44736560e74cae4c953963fb310a9bd606886ac6
PratikChakraborty10 <chakrabortypratik2000@gmail.com>
2021-06-29 18:26:38 +0530

    Quick Sort Code Optimised

--- Recursion/quickSort.cpp
+++ Recursion/quickSort.cpp
@@ -1,33 +1,34 @@
+/*
+    Time Complexity = O(n * log(n))
+    Space Complexity = O(n)
+    where n is the size of the array arr[]
+*/
 #include<iostream>
 using namespace std;
-// Here we are taking the first / start index element as pivot
 int partition(int arr[], int si, int ei) {
-    int pivot = arr[si];             // Taking the start element as pivot
-    int countSmaller = 0;            // Counting all the elements smaller than the pivot element
-    for(int i=si;i<=ei;i++) {
-        if(arr[i] < pivot) {
-            countSmaller++;
+    int pivot = arr[si];        // Assuming the si of the array as pivot 
+    int smallCount = 0;         // Counting the elements smaller than the pivot element
+    for(int i=si+1;i<=ei;i++) {
+        if(arr[i] <= pivot) {
+            smallCount++;            
         }
-    }                                // Finally stored all the count of all smaller elements in countSmaller 
-    int pivotIndex = si + countSmaller;     // Shifting pivot element i.e., 'si' to the pivotIndex, by leaving the space for all the smaller element less that pivot element
+    } 
+    // Stored the total number of elements smaller than arr[si] on smallCount
+    int pivotIndex = si + smallCount;     // Getting the pivotIndex, where pivot element is to be stored
+    arr[si] = arr[pivotIndex];            // Shifing the pivot element i.e., arr[si] to its correct position by swapping the element at that position to arr[si]
+    arr[pivotIndex] = pivot;             // Pivot element is now shifted to pivotIndex i.e., to its correct position 
 
-    // Swapping the element already present in pivotIndex with arr[si], i.e., pivot element
-    int temp = arr[si];
-    arr[si] = arr[pivotIndex];
-    arr[pivotIndex] = temp;         // pivot element has been sent to its correct position by leaving space for elements smaller that pivot element
+    // Ensure left half contains elements smaller that pivot elemet and rignt half contains eleements laeger than pivot
 
-
-    // Ensures left half contains elements smaller than pivot and right half contains elements greater than pivot
     int i = si;
     int j = ei;
     while(i <= pivotIndex && j > pivotIndex) {
-        while(arr[i] <= pivot) {
+        if(arr[i] <= pivot) {
             i++;
         }
-        while(arr[j] > pivot) {
+        else if(arr[j] > pivot) {
             j--;
-        }
-        if(i < pivotIndex && j > pivotIndex) {
+        } else {
             int temp = arr[i];
             arr[i] = arr[j];
             arr[j] = temp;
@@ -35,11 +36,9 @@ int partition(int arr[], int si, int ei) {
             j--;
         }
     }
-
     return pivotIndex;
-    
 }
-void quick_sort(int arr[], int si, int ei) {
+void quick_sort_help(int arr[], int si, int ei) {
     // Base Case
     if(si >= ei) {
         return;
@@ -47,14 +46,14 @@ void quick_sort(int arr[], int si, int ei) {
     // Recursive Call to do partition of the array
     int p = partition(arr, si, ei);
     // Now p stores the position of the pivotIndex after partition
-    quick_sort(arr, si, p-1);     // Sorting the left half untill pivotIndex
-    quick_sort(arr, p+1, ei);     // Sorting the right half after pivotIndex
-    
+    quick_sort_help(arr, si, p-1);              // Sorting the left half untill pivotIndex
+    quick_sort_help(arr, p+1, ei);              // Sorting the right half after pivotIndex
 }
 void quickSort(int arr[], int size) {
-    // Recursive call for the same quick_sort() function that contains the size of array for custom input
-    quick_sort(arr, 0, size-1);
+    // Recursive call
+    quick_sort_help(arr, 0, size-1);
 }
+// Driver Code
 int main() {
     int size;
     cin >> size;
