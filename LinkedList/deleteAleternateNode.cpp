4fe20e8e20049f866dc3f2b1c5b72ba0af2c8446
PratikChakraborty10 <chakrabortypratik2000@gmail.com>
2021-10-09 00:27:27 +0530

    delete alternate node

---
+++ LinkedList/deleteAleternateNode.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std;
+class Node {
+    public:
+    int data;
+    Node *next;
+    Node(int data) {
+        this->data=data;
+        this->next=NULL;
+    }
+    ~Node() {
+        if(next) {
+            delete next;
+        }
+    }
+};
+Node *takeInput() {
+    int data;
+    cin >> data;
+    Node *head = NULL;
+    Node *tail = NULL;
+    while(data != -1) {
+        Node *newNode = new Node(data);
+        if(head == NULL) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail->next=newNode;
+            tail=newNode;
+        }
+        cin >> data;
+    }
+    return head;
+}
+void deleteAlternateNode(Node *head) {
+    while(head->next != NULL) {
+        if(head->next->next != NULL) {
+            head->next = head->next->next;
+            head=head->next;
+        } else {
+            head->next = NULL;
+        }
+    }
+}
+void print(Node *head) {
+    Node *temp = head;
+    while(temp != NULL) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+int main() {
+    Node *head = takeInput();
+    deleteAlternateNode(head);
+    print(head);
+}
---
+++ LinkedList/midPointLL.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std;
+class Node {
+    public:
+    int data;
+    Node *next;
+    Node(int data) {
+        this->data=data;
+        this->next=NULL;
+    }
+};
+Node *takeInput() {
+    int data;
+    cin >> data;
+    Node *head = NULL;
+    Node *tail = NULL;
+    while(data != -1) {
+        Node *newNode = new Node(data);
+        if(head == NULL) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail->next=newNode;
+            tail=newNode;
+        }
+        cin >> data;
+    }
+    return head;
+}
+Node *midPoint(Node *head) {
+    if(head==NULL || head->next==NULL) {
+        return head;
+    }
+    Node *slow = head;
+    Node *fast = head->next;
+    while(fast != NULL || fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow;
+}
+int main() {
+    int t;
+	cin >> t;
+	while (t--)
+	{
+		Node *head = takeInput();
+		Node *mid = midPoint(head);
+		if (mid != NULL)
+		{
+			cout << mid->data;
+		}
+		cout << endl;
+	}
+	return 0;
+}
